70a558d6f0ccd49bde9ab8d6b1ef27b048ff306c
gru13 <guruprasath1302@gmail.com>
2024-04-02 08:40:15 +0530

    formated the code

--- codes/relay_controller/relay_controller.ino
+++ codes/relay_controller/relay_controller.ino
@@ -1,4 +1,4 @@
-int relay_pins[] = {6,7,8,9,10,11,5};
+int relay_pins[] = {6, 7, 8, 9, 10, 11, 5};
 int SIZE = 7;
 char out;
 
@@ -10,21 +10,21 @@ void setup(){
   Serial.begin(9600);
   Serial.flush();
   out = 0;
-  for(int i = 0;i<SIZE;i++){
-    pinMode(relay_pins[i],OUTPUT);
-    digitalWrite(relay_pins[i],HIGH);
+  for(int i = 0; i < SIZE; i++){
+    pinMode(relay_pins[i], OUTPUT);
+    digitalWrite(relay_pins[i], HIGH);
   }
 }
 
 void loop() {   
   if (Serial.available() >= 1) {
     out = Serial.read();
-    Serial.println(out,DEC);
-    for(int i = SIZE;i>=0;i--){
+    Serial.println(out, DEC);
+    for(int i = SIZE; i >= 0; i--){
         if(getBit(i) == 1){
-            digitalWrite(relay_pins[i],LOW);
+            digitalWrite(relay_pins[i], LOW);
         }else{
-            digitalWrite(relay_pins[i],HIGH);
+            digitalWrite(relay_pins[i], HIGH);
         }
     } 
     out =0;
